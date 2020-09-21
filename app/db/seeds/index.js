9742da87a330a16e4bbbd9d41d201a7c19094f25
kevinconvery <kevinconvery@gmail.com>
2020-09-21 16:25:43 -0400

    create seed for db reset route

---
+++ app/db/seeds/index.js
@@ -0,0 +1,24 @@
+const jokeSeedData = [
+  {
+    opening: `I've been thinking of selling a home surgical kit.`,
+    punchline: `I'll call it "Suture Self".`
+  },
+  {
+    opening: `Why couldn't the green pepper practice archery?`,
+    punchline: `He didn't habanero.`
+  },
+  {
+    opening: `I spent time copying six pages of the dictionary today.`,
+    punchline: `I learned next to nothing.`
+  },
+  {
+    opening: `Police have arrested the world tongue-twister champion.`,
+    punchline: `I imagine she'll be given a long sentence.`
+  },
+  {
+    opening: `Stadiums are bound to be hotter this season.`,
+    punchline: `They'll be missing all their fans.`
+  }
+]
+
+module.exports = { jokeSeedData }
