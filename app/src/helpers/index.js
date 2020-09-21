4d4151fac887745f1cdb4461e422195edc17e508
kevinconvery <kevinconvery@gmail.com>
2020-09-21 15:40:53 -0400

    move hard data out of helpers

--- app/src/helpers/index.js
+++ app/src/helpers/index.js
@@ -1,26 +1,3 @@
-export const jokeList = [
-  {
-    intro: `I've been thinking of selling a home surgical kit.`,
-    punchline: `I'll call it "Suture Self".`
-  },
-  {
-    intro: `Why couldn't the green pepper practice archery?`,
-    punchline: `He didn't habanero.`
-  },
-  {
-    intro: `I spent time copying six pages of the dictionary today.`,
-    punchline: `I learned next to nothing.`
-  },
-  {
-    intro: `Police have arrested the world tongue-twister champion.`,
-    punchline: `I imagine she'll be given a long sentence.`
-  },
-  {
-    intro: `Stadiums are bound to be hotter this season.`,
-    punchline: `They'll be missing all their fans.`
-  }
-]
-
 export const getNewJoke = (all, current) => {
   const jokes = all.filter(joke => joke !== current)
   const newJoke = jokes[Math.floor(Math.random() * jokes.length)]
