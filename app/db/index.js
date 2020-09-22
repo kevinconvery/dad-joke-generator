343142f9195477afb3143e08f9de9e0b48d95dc9
kevinconvery <kevinconvery@gmail.com>
2020-09-22 12:05:40 -0400

    move route code out of index.js file

--- app/db/index.js
+++ app/db/index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-app.use(express.json())
 const port = 7000
-const { Joke } = require('./models')
+const { dataRoute } = require('./routes')
 const dbConnectionObject = {
   endpoint: `mongodb://localhost/dad-jokes-forever`,
   params: {
@@ -20,25 +19,9 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'))
 db.once('open', () => console.log('Connected to MongoDB database!'))
 
-app.get('/', (req, res) => {
-  res.send(`We're connected!`)
-})
-
-app.get('/data', async (req, res) => {
-  console.log('route hit')
-  const results = await Joke.find()
-  res.send(results)
-})
-
-app.post('/data', async (req, res) => {
-  const { opening, punchline } = req.body
-  const newJoke = new Joke({ opening: opening, punchline: punchline })
-  try {
-    newJoke.save()
-  } catch(err) {
-    console.log(err)
-  }
-})
+app.use(express.json())
+// mount our routes
+app.use('/data', dataRoute)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
